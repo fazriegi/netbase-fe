e8efa6968e398356e7a6a9f4a4cf14f9bbaaeb97
fazriegi <fazriegi80@gmail.com>
2026-08-28 20:16:41 +0700

    feat: configure assets directory to static in vite build settings

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsDir: "static",
+  },
   resolve: {
     alias: {
       src: path.resolve(__dirname, "src"),
